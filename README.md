29189acab6b836c2675954ae56bd0faef09f3528
Jonah Miller <jojojrman@gmail.com>
2020-12-16 12:23:37 -0500

    Addd comments and README

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Password-Gen
 Password Generation
+This Password generator asks the user for specific criteria for the password:
+------------------------------
+* Special characters ex, !@#$;
+* Numbers ex, 1234;
+* Caps ex ABCD;
+If the user chooses one it'll get added to the main charset.
+------------------------------
+Confirms are used to see what special char is needed for the password.
+length is prompted after the confirms to ask the user how long the password needs to be from 8-128 characters.
+------------------------------
+# For potential debugging
+------------------------------
+* console.log is added for the confirms as well as the charset;
+* These are in the inspect chrome dev tools, under console;
+* For the confirms it will show True or False;
+* For the 
--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
+// Password Generation 
 function generatePassword() {
-
+// Special Confirms
   var specialCr = confirm("Special Characters?");
   var specialNr = confirm("Numbers?");
   var specialCp = confirm("Capitol letters?");
-
+// logged confirms for potential debugging
   console.log(specialCr);
   console.log(specialNr);
   console.log(specialCp);
-
+// Length of password 8-128
   var length = prompt("How Long of a password? 8-128");
   if (length < "8" || length > "128") {
     length = prompt("Please pick a number between 8 and 128");
   }
-
+// Special charsets to be added if prompts were selected
   var special1 = "!#$%&'()*+";
   var special2 = "0123456789";
   var special3 = "QRSTUVWXYZ";
   var charset = "abcdefghij";
-
+//Adding to the charsets depending on user selection
   if (specialCr === true) {
     charset = charset+special1;
   } 
@@ -34,13 +34,15 @@ function generatePassword() {
   if (specialCr === false && specialNr === false && specialCp === false) {
     charset = "abcdefghijklmnopqrstuvwxyz";
   }
-
+// Logged charset for potential debugging
   console.log(charset);
+// Password var
   var retVal = "";
-
+// Random generation for password
   for (var i = 0, n = charset.length; i < length; i++) {
       retVal += charset.charAt(Math.floor(Math.random() * n));
   }
+// Final password returned
   return retVal;
 }
 // Write password to the #password input
